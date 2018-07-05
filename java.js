f7326bd2668b1f58439084d85d7a1633e383530e
Erica <e.izaguirre0112@gmail.com>
2018-07-05 11:05:40 -0400

    second commit

---
+++ java.js
@@ -0,0 +1,13 @@
+/*{oVo}
+<audio id="sax">
+  <source src="sax.mp3" type="audio/mpeg">
+  Sorry m8 no dice.
+</audio>
+<button onclick="playAudio()" type="button">Do the deed</button>
+
+*/
+// var x = document.getElementById("mysax"); 
+//
+// function playAudio() {
+//     x.play();
+// }
